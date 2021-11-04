9d5120e299c07813259692fe1d29a7f24aa67b2f
Alex2114011 <Alex2114011@gmail.com>
2021-11-04 18:14:29 +0300

    Add ListTableView
    Добавил таблицу

--- FinchViperTask/Modules/ListScreen/View/ListScreenViewController.swift
+++ FinchViperTask/Modules/ListScreen/View/ListScreenViewController.swift
@@ -15,19 +15,50 @@ final class ListScreenViewController: UIViewController {
     var presenter: ListScreenViewOutput?
 
     // MARK: - Private Properties
+    private var listTableView = UITableView()
 
     // MARK: - LifeCircle
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        setupTable()
     }
 
     // MARK: - Private Methods
-
+    private func setupTable() {
+        // Delegate
+        listTableView.dataSource = self
+        listTableView.delegate = self
+        // SetupUI Table
+        view.addSubview(listTableView)
+        listTableView.translatesAutoresizingMaskIntoConstraints = false
+        listTableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        listTableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        listTableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        listTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        // Register cell
+        listTableView.register(cell: ListTableViewCell.self)
+        listTableView.separatorStyle = .none
+    }
 }
 
 // MARK: - ListScreenViewInput
 extension ListScreenViewController: ListScreenViewInput {
+
+}
+
+// MARK: - UITableViewDataSource
+extension ListScreenViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.getCell(ListTableViewCell.self, indexPath)
+        cell.selectionStyle = .none
+        return cell
+    }
+}
+// MARK: - UITableViewDelegate
+extension ListScreenViewController: UITableViewDelegate {
 
 }
