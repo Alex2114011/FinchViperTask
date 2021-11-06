1bd4cb9ad65c83f191144ceffa76879c569bc4d1
Alex2114011 <Alex2114011@gmail.com>
2021-11-06 10:32:09 +0300

    Add Entity(Model)

---
+++ FinchViperTask/Entity/Note.swift
@@ -0,0 +1,14 @@
+//
+//  Note.swift
+//  FinchViperTask
+//
+//  Created by Alex on 06.11.2021.
+//
+
+import Foundation
+
+struct Note: Codable {
+    var imageNote: Data
+    var titleNote: String
+    var descriptionNote: String
+}
